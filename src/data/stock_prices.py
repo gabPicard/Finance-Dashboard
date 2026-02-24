1ae56815608504070d8f17137207af7109a58708
Gabriel Picard <gab_picard@yahoo.com>
2026-02-24 13:53:17 +0100

    /fix Documentation

--- src/data/stock_prices.py
+++ src/data/stock_prices.py
@@ -3,19 +3,31 @@ import os
 from .data_engineering import validate_and_clean_data, fix_price_anomalies
 from .fetch_data import fetch_stock_data, fetch_risk_free_rate
 
-def get_stock_prices(market, 
+def get_stock_prices(market: str, 
                      start_date=None, 
                      end_date=None, 
                      period="1y", 
                      interval="1d",
-                     columns=['Close']):
+                     columns=['Close']
+                    ) -> dict | float:
     """
     Get clean and validate historical stock prices and the risk free rate.
     
     :param market: Must be the name of a known market in the json file.
     :type market: str
+    :param start_date: [Optional] The starting point for fetching. If None, will use period to compute the start date
+    :type start_date: str
+    :param end_date: [Optional] The end point for fetching. By default, today
+    :type end_date: str
+    :param period: [Optional] The period for fetching. By default, 1 year
+    :type period: str
+    :param interval: [Optional] The interval to fecth data. By default, 1 day
+    :type interval: str
 
-    :returns clean_prices | risk free rate:
+    :returns clean_prices: ['Date', 'Ticker', 'Open', 'High', 'Low', 'Close', 'Volume']
+    :rtype clean_prices: dict
+    :returns risk_free_rate: The risk free return rate
+    :rtype rirks_free_rate: float
     """
     
     tickers_list, risk_free_rate_ticker = get_tickers_list(market)
