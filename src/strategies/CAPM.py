84d0743d13b2cc2e481a36f1aae792425b5498d5
Gabriel Picard <gab_picard@yahoo.com>
2026-03-11 08:57:11 +0100

    /feature CAPM functions

--- src/strategies/CAPM.py
+++ src/strategies/CAPM.py
@@ -1,10 +1,9 @@
-import numpy as np
 import pandas as pd
 
 def calculate_beta(asset_returns: pd.Series,
                    market_returns: pd.Series
                 ) -> float:
-    """
+   """
     Compute the Beta of the asset
 
     :param asset_returns: Historical returns of the asset
@@ -15,10 +14,38 @@ def calculate_beta(asset_returns: pd.Series,
     :returns beta: The asset's exposure to the market
     :rtype beta: float
     """
-    covariance = asset_returns.cov(market_returns)
-    market_var = market_returns.var()
+   covariance = asset_returns.cov(market_returns)
+   market_var = market_returns.var()
 
-    beta = covariance/market_var
+   beta = covariance/market_var
 
-    return beta
+   return beta
 
+def capm_expected_returns(prices: pd.DataFrame,
+                          market_prices: pd.DataFrame,
+                          risk_free_rate: float=0.04
+                        ) -> pd.Series:
+   """
+   Compute the expected returns of assets using the CAPM
+
+   :param prices: The assets' historical prices
+   :type prices: pd.DataFrame
+   :param market_prices: The market historical prices
+   :type market_prices: pd.DatFrame
+   :param risk_free_rate: The risk-free return rate. By default 4%
+   :type risk_free_rate: float
+
+   :returns capm_returns: dict{asset: capm_expected_return}
+   :rtype capm_returns: dict
+   """
+   market_returns = market_prices.pct_change()
+   asset_returns = prices.pct_change()
+
+   market_expected_return = market_returns.mean() * 252
+
+   capm_returns = {}
+   for asset in asset_returns.columns:
+      beta_i = calculate_beta(asset_returns[asset], market_returns)
+      capm_returns[asset] = risk_free_rate + beta_i * (market_expected_return - risk_free_rate)
+   
+   return capm_returns
